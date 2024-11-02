48312950bf301f7ad478c14882c916a70534f6d8
Jake <jakecast810@gmail.com>
2024-11-02 19:32:48 -0400

    created db models

---
+++ Tools537Project/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from ..accounts.models import CustomUser
+
+class Survey(models.Model):
+	Suvery_name = models.CharField(max_length=200)
+	Survey_description = models.CharField(max_length=200)
+	State = models.CharField(max_length=20)
+	Time_created = models.DateTime()
+	Creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+class Questions(models.Model):
+	Question_description = models.CharField(max_length=200)
+	Question_type = models.CharField(max_length=10)
+	Survey_from = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+
+class Answers(models.Model):
+	Answer = models.CharField(max_length=200)
+	Question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+	Survey_for = models.ForeignKey(Survey, on_delete=models.CASCADE)
--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     pass
-    
+    role = models.CharField(max_length=7)
 
     def __str__(self):
         return self.username
