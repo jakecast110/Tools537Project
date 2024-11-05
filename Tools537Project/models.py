02ee67a46f7e4211e4a0173aada01aebd1c9db9a
Jake <jakecast810@gmail.com>
2024-11-04 22:06:32 -0500

    change accounts directory and corrected DateTimeField

--- Tools537Project/models.py
+++ Tools537Project/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from ..accounts.models import CustomUser
+from accounts.models import CustomUser
 
 class Survey(models.Model):
 	Suvery_name = models.CharField(max_length=200)
 	Survey_description = models.CharField(max_length=200)
 	State = models.CharField(max_length=20)
-	Time_created = models.DateTime()
+	Time_created = models.DateTimeField()
 	Creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
 class Questions(models.Model):
