7f906220656d9c34b6162a4168f5dcd20b17e63e
Jake <jakecast810@gmail.com>
2024-11-04 21:48:57 -0500

    set up database authentication

--- Tools537Project/settings.py
+++ Tools537Project/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'survey_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ToolsProject',
+        'USER': 'postgres',
+        'PASSWORD': 'Pineapple110!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
