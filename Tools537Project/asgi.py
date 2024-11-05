656a7b258cccb58bf289450c04e6eeaf1b89afaa
Jake <jakecast810@gmail.com>
2024-11-04 21:57:19 -0500

    set correct directory name

--- Tools537Project/asgi.py
+++ Tools537Project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'survey_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tools537Project.settings')
 
 application = get_asgi_application()
