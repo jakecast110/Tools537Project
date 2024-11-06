6852a39ffc341eb2a2bd45f3b45eed2a4a5017c9
Jake <jakecast810@gmail.com>
2024-11-06 18:31:08 -0500

    create views for serializing request data

--- Tools537Project/views.py
+++ Tools537Project/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from Tools537Project.models import Survey, Questions, Answers
+from Tools537Project.serializers import SurveySerializer
 
-def showSurveys(request):
-    return render(request, 'survey_dashboard.html')
+@csrf_exempt
+def survey_list(request):
+    if request.method == 'GET':
+        surveys = Survey.objects.all()
+        serializer = SurveySerializer(surveys, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SurveySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+@csrf_exempt
+def survey_detail(request, pk):
+    try:
+        survey = Survey.objects.get(pk=pk)
+    except Survey.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = SurveySerializer(survey)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SurveySerializer(survey, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
 
-def createSurvey(request):
-    return render(request, 'create_survey.html')
+    elif request.method == 'DELETE':
+        survey.delete()
+        return HttpResponse(status=204)
