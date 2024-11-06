0e01dbed3de32c6bb802dc88df9401c42df097c9
Jake <jakecast810@gmail.com>
2024-11-05 22:29:18 -0500

    Start survey creation page and reformat survey dashboard table

--- Tools537Project/urls.py
+++ Tools537Project/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('surveydashboard/', views.showSurveys, name='showSurveys'),
+    path('createsurvey', views.createSurvey, name='createSurvey'),
 ]
--- Tools537Project/views.py
+++ Tools537Project/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def showSurveys(request):
     return render(request, 'survey_dashboard.html')
+
+def createSurvey(request):
+    return render(request, 'create_survey.html')
