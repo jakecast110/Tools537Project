6d1cb03d5e6be61455783f89b7938c1b490e1b16
Jake <jakecast810@gmail.com>
2024-11-06 18:31:28 -0500

    make survey time created field auto generate

--- Tools537Project/models.py
+++ Tools537Project/models.py
@@ -5,7 +5,7 @@ class Survey(models.Model):
 	Survey_name = models.CharField(max_length=200)
 	Survey_description = models.CharField(max_length=200)
 	State = models.CharField(max_length=20)
-	Time_created = models.DateTimeField()
+	Time_created = models.DateTimeField(auto_now=True)
 	Creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
 class Questions(models.Model):
