57a48fa48ee6b921beaef74e4336cd8ae1b53f65
Jake <jakecast810@gmail.com>
2024-11-06 19:56:41 -0500

    cleaned code

--- Tools537Project/views.py
+++ Tools537Project/views.py
@@ -1,46 +1,11 @@
 from Tools537Project.models import Survey
 from Tools537Project.serializers import SurveySerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
-class SurveyList(APIView):
-
-    def get(self, request, format=None):
-        surveys = Survey.objects.all()
-        serializer = SurveySerializer(surveys, many=True)
-        return Response(serializer.data)
+class SurveyList(generics.ListCreateAPIView):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
         
-    def post(self, request, format=None):
-        serializer = SurveySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-class SurveyDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            survey = Survey.objects.get(pk=pk)
-        except Survey.DoesNotExist:
-            return Http404
-    
-    def get(self, request, pk, format=None):
-        survey=self.get_object(pk)
-        serializer = SurveySerializer(survey)
-        return Response(serializer.data)
-    
-    def put(self, request, pk, format=None):
-        survey = self.get_object(pk)
-        serializer = SurveySerializer(survey, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        survey=self.get_object(pk)
-        survey.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class SurveyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
